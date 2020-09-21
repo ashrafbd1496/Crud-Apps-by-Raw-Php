00960e881ab2bc729f6b7ce63cf57bb01408e3ca
ashrafuddin1496 <asraf1496@gmail.com>
2020-09-21 10:31:52 +0600

    function added

--- myphp.php
+++ myphp.php
@@ -1,4 +1,18 @@
 <?php  
 
+function myfunction (){
+    $first_value =  10;
+    $second_value = 52;
+
+    $total = $first_value + $second_value;
+
+    echo $total;
+}
+
 echo "this is the test integration of github with local storage"
-;?>
+;
+
+echo myfunction();
+
+
+?>
