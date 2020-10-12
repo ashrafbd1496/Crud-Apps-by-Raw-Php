29a68740e816de45d1b5897a39d71168580128a2
ashrafuddin1496 <asraf1496@gmail.com>
2020-10-12 08:59:12 +0600

    all files added

---
+++ app/db.php
@@ -0,0 +1,19 @@
+<?php 
+
+	/**
+	*Database connection
+
+	*/
+
+	$host = 'localhost';
+	$user = 'root';
+	$pass = '';
+	$db = 'day10crudapps';
+
+
+
+
+	$connection = new mysqli($host, $user, $pass, $db); 
+	
+
+ ?>
